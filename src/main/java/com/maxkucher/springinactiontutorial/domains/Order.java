36f3cb4cea4c67dba4c9957947a53d488d1f3c61
Maksym Kucher <the.stewk@gmail.com>
2019-10-22 08:32:11 -0700

    Finished taco model

--- src/main/java/com/maxkucher/springinactiontutorial/domains/Order.java
+++ src/main/java/com/maxkucher/springinactiontutorial/domains/Order.java
@@ -6,6 +6,7 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -27,18 +28,16 @@ public class Order {
     private String zip;
     @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
-    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
-            message="Must be formatted MM/YY")
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+            message = "Must be formatted MM/YY")
     private String ccExpiration;
-    @Digits(integer = 3, fraction = 0,message = "Invalid CC")
+    @Digits(integer = 3, fraction = 0, message = "Invalid CC")
     private String ccCVV;
 
+    private List<Taco> tacos = new ArrayList<>();
 
     public void addDesign(Taco saved) {
-
+        tacos.add(saved);
     }
 
-    public List<Taco> getTacos() {
-        return null;
-    }
 }
