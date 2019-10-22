1063b90b40aff97012c1acad4639374c2dc87393
Maksym Kucher <the.stewk@gmail.com>
2019-10-22 13:31:17 -0700

    added jpa annotation to entities

--- src/main/java/com/maxkucher/springinactiontutorial/controllers/DesignTacoController.java
+++ src/main/java/com/maxkucher/springinactiontutorial/controllers/DesignTacoController.java
@@ -6,7 +6,6 @@ import com.maxkucher.springinactiontutorial.domains.Order;
 import com.maxkucher.springinactiontutorial.domains.Taco;
 import com.maxkucher.springinactiontutorial.repositories.IngredientRepository;
 import com.maxkucher.springinactiontutorial.repositories.TacoRepository;
-import com.sun.org.apache.xpath.internal.operations.Or;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -64,23 +63,19 @@ public class DesignTacoController {
     @GetMapping
     public String showDesignForm(Model model) {
         List<Ingredient> ingredients = new ArrayList<>();
-        ingredientRepository.findAll()
-                .forEach(ingredients::add);
+        ingredientRepository.findAll().forEach(ingredients::add);
 
         Ingredient.Type[] types = Ingredient.Type.values();
         for (Ingredient.Type type : types) {
-            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
         }
-        model.addAttribute("design", new Taco());
+
+        model.addAttribute("design",new Taco());
+
         return "design";
     }
 
-
-    @PostMapping
-    public String processDesign(Taco design) {
-        log.info("Processing design {}", design.toString());
-        return "redirect:/orders/current";
-    }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Ingredient.Type type) {
         return ingredients
--- src/main/java/com/maxkucher/springinactiontutorial/domains/Order.java
+++ src/main/java/com/maxkucher/springinactiontutorial/domains/Order.java
@@ -3,16 +3,23 @@ package com.maxkucher.springinactiontutorial.domains;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
-public class Order {
+@Entity
+@Table(name = "Taco_Order")
+public class Order implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Date placedAt;
 
@@ -34,10 +41,20 @@ public class Order {
     @Digits(integer = 3, fraction = 0, message = "Invalid CC")
     private String ccCVV;
 
+
+
+    @ManyToMany(targetEntity = Taco.class)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addDesign(Taco saved) {
         tacos.add(saved);
+    }
+
+
+    @PrePersist
+    void placeAt()
+    {
+        this.placedAt = new Date();
     }
 
 }
