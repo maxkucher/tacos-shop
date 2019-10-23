9d71a9d751c819f89f66bc2e99833d6067356461
Maksym Kucher <the.stewk@gmail.com>
2019-10-23 10:02:11 -0700

    added delete order endpoint.

--- src/main/java/com/maxkucher/springinactiontutorial/controllers/OrderRestController.java
+++ src/main/java/com/maxkucher/springinactiontutorial/controllers/OrderRestController.java
@@ -4,8 +4,11 @@ package com.maxkucher.springinactiontutorial.controllers;
 import com.maxkucher.springinactiontutorial.domains.Order;
 import com.maxkucher.springinactiontutorial.repositories.OrdersRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -16,20 +19,26 @@ public class OrderRestController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Order createOrder(@RequestBody Order order)
-    {
+    public Order createOrder(@RequestBody Order order) {
         return ordersRepository
                 .save(order);
     }
 
     @PutMapping
-    public Order putOrder(@RequestBody Order order){
+    public Order putOrder(@RequestBody Order order) {
         return ordersRepository.save(order);
     }
 
+    @DeleteMapping("/{id}")
+    public void deleteOrder(@PathVariable Long id, HttpServletResponse response) {
+        try {
+            ordersRepository.deleteById(id);
+            response.setStatus(HttpStatus.NO_CONTENT.value());
+        } catch (EmptyResultDataAccessException e) {
+            response.setStatus(HttpStatus.NOT_FOUND.value());
+        }
 
-
-
+    }
 
 
 }
