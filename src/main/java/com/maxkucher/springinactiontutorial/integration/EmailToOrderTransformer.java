cabffe93a878000c778bdc13ab9f87c1f6d06653
Maksym Kucher <the.stewk@gmail.com>
2019-10-25 08:40:30 -0700

    transformer will now transform email to order integration dto, which will contain only list of tacos and user's email.

--- src/main/java/com/maxkucher/springinactiontutorial/integration/EmailToOrderTransformer.java
+++ src/main/java/com/maxkucher/springinactiontutorial/integration/EmailToOrderTransformer.java
@@ -11,17 +11,17 @@ import javax.mail.Message;
 
 @Component
 public class EmailToOrderTransformer
-extends AbstractMailMessageTransformer<Order> {
+        extends AbstractMailMessageTransformer<OrderIntegrationDto> {
 
 
     @Override
-    protected AbstractIntegrationMessageBuilder<Order> doTransform(javax.mail.Message message) throws Exception {
-        Order order = processPayload(message);
+    protected AbstractIntegrationMessageBuilder<OrderIntegrationDto> doTransform(javax.mail.Message message) throws Exception {
+        OrderIntegrationDto order = processPayload(message);
         return MessageBuilder.withPayload(order);
     }
 
     // TODO: 10/25/2019 implement
-    private Order processPayload(Message message) {
+    private OrderIntegrationDto processPayload(Message message) {
         throw new NotImplementedException();
     }
 }
